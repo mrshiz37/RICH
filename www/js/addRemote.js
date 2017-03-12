c96fc3d4648f4562ee3036cf6846ea2c243eb694
jacobklitzke <jacobklitzke@gmail.com>
2017-03-12 18:56:55 -0500

    Updated record remote module

--- www/js/addRemote.js
+++ www/js/addRemote.js
@@ -1,8 +1,8 @@
 angular.module('addRemote', ['nodes'])
     .controller('addRemoteCtrl', function($scope, $http, $state, nodeService) {
-        nodeService.getNodes(function(nodes) {
+        /*nodeService.getNodes(function(nodes) {
             $scope.nodes = nodes;
-        });
+        });*/
 
         $scope.formData = {};
         $scope.custom_name = "";
@@ -41,54 +41,5 @@ angular.module('addRemote', ['nodes'])
 
         $scope.go = function(path) {
             $state.go(path);
-        };
-    })
-    .controller('recordRemoteCtrl', function($scope, $http, $location) {
-        $http.get('/files/lirc_namespace.json').success(function(data) {
-            $scope.buttons = data;
-        });
-        $scope.buttonStep = 0;
-        $scope.fromBackend = 'When you are ready to begin, press the button below.';
-        var counter = 0;
-        $scope.selectedButton = '';
-        $scope.buttonState = [{
-                step: 0,
-                state: "Begin"
-            },
-            {
-                step: 1,
-                state: "Press Enter"
-            },
-            {
-                step: 2,
-                state: "Press Enter after selecting"
-            },
-            {
-                step: 3,
-                state: "Finished"
-            }
-        ];
-
-        $scope.updateHtml = function() {
-            if ($scope.buttonStep < $scope.buttonState.length - 1) {
-                $scope.buttonStep++;
-                collectBackend();
-            }
-        };
-
-        function collectBackend() {
-            $http.get('addRemoteBackend', {
-                params: {
-                    selected: $scope.selectedButtond
-                }
-            }).success(function(data) {
-                $scope.fromBackend = data + counter;
-                counter++;
-            });
-        }
-    })
-    .directive('remoteSelect', function() {
-        return {
-            template: "<h1>Made by a directive!</h1>"
         };
     });
